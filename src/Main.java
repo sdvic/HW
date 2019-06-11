39f2b5c804222ff183740ce95aba95fbffff9882
Vic Wintriss <sdvic@me.com>
2019-06-10 20:24:40 -0700

    /***********************************************************************  * Full Swing Golf Strip Test version 0.1, 6/10/2019  * Raspberry Pi...can't get pi4j library attached  * copyright 2019 Vic Wintriss  ***********************************************************************/  Working without pi4j

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,25 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import static java.awt.Toolkit.*;
+//import com.pi4j.io.gpio.GpioController;
+//import com.pi4j.io.gpio.GpioFactory;
+//import com.pi4j.io.gpio.GpioPin;
+//import com.pi4j.io.gpio.GpioPinDigitalInput;
+//import com.pi4j.io.gpio.GpioPinDigitalOutput;
+//import com.pi4j.io.gpio.PinDirection;
+//import com.pi4j.io.gpio.PinMode;
+//import com.pi4j.io.gpio.PinPullResistance;
+//import com.pi4j.io.gpio.PinState;
+//import com.pi4j.io.gpio.RaspiPin;
+//import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+//import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
+//import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+//import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+//import com.pi4j.io.gpio.event.GpioPinListener;
+//import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+//import com.pi4j.io.gpio.event.GpioPinEvent;
+//import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+//import com.pi4j.io.gpio.event.PinEventType;
 
 /***********************************************************************
  * Full Swing Golf Strip Test version 0.0, 6/8/2019
@@ -12,6 +31,13 @@ public class Main extends JComponent implements Runnable, ActionListener
 {
     Timer paintTicker = new Timer(20, this);
     String jopInput = "";
+//    final GpioController gpio = GpioFactory.getInstance();
+//    private GpioPinDigitalOutput strobeLeft = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);// Strobe...pin 11
+//    private GpioPinInput echoLeft = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03); // Echo...pin 15
+//    private GpioPinDigitalOutput strobeRight = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);// Strobe...pin 12
+//    private GpioPinInput echoRight = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04); // Echo...pin 16
+//    private GpioPinDigitalOutput strobeCenter = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);// Strobe...pin 13
+//    private GpioPinInput echoCenter = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05); // Echo...pin 18
 
     public static void main(String[] args)
     {
@@ -29,7 +55,28 @@ public class Main extends JComponent implements Runnable, ActionListener
         gameWindow.getContentPane().setBackground(new Color(200, 235, 255));
         gameWindow.setVisible(true);
         paintTicker.start();
+        sonar();
     }
+
+    public void sonar()
+    {
+        System.out.println("Starting Sonar");
+//        echoLeft.addListener(new GpioPinListenerDigital()
+//        {
+//            @Override
+//            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
+//            {
+//                if (event.getState() == PinState.HIGH)
+//                {
+//                }
+//                if (event.getState() == PinState.LOW)
+//                {
+//                }
+//
+//            }
+//        });
+    }
+
 
     public void paint(Graphics g)
     {
