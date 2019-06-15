694f50d7ac50060709738df5ffbec20c26d09fdd
Vic Wintriss <sdvic@me.com>
2019-06-15 15:28:03 -0700

    Wlorks with RPI...graphics shapes...ver 190615

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
+import com.pi4j.io.gpio.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import static java.awt.Toolkit.*;
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPin;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static java.awt.Toolkit.getDefaultToolkit;
 //import com.pi4j.io.gpio.PinDirection;
 //import com.pi4j.io.gpio.PinMode;
 //import com.pi4j.io.gpio.PinPullResistance;
@@ -24,67 +26,60 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 //import com.pi4j.io.gpio.event.PinEventType;
 
 /***********************************************************************
- * Full Swing Golf Strip Test version 0.0, 6/8/2019
+ * Full Swing Golf Strip Test version 0.9, 6/14/2019
  * copyright 2019 Vic Wintriss
  ***********************************************************************/
-public class Main extends JComponent implements Runnable, ActionListener
+public class Main extends JComponent implements ActionListener
 {
+    final GpioController gpio = GpioFactory.getInstance();
+    final GpioPinDigitalOutput pinA = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
     Timer paintTicker = new Timer(20, this);
     String jopInput = "";
-//    final GpioController gpio = GpioFactory.getInstance();
-//    private GpioPinDigitalOutput strobeLeft = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);// Strobe...pin 11
+
+
 //    private GpioPinInput echoLeft = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03); // Echo...pin 15
 //    private GpioPinDigitalOutput strobeRight = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);// Strobe...pin 12
 //    private GpioPinInput echoRight = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04); // Echo...pin 16
 //    private GpioPinDigitalOutput strobeCenter = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);// Strobe...pin 13
 //    private GpioPinInput echoCenter = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05); // Echo...pin 18
 
+
     public static void main(String[] args)
     {
-        SwingUtilities.invokeLater(new Main());
+        new Main().getGoing();
     }
 
-    @Override
-    public void run()
+    public void getGoing()
     {
-        jopInput =JOptionPane.showInputDialog("What do you want me to do?");
         JFrame gameWindow = new JFrame("FSG StripTest");
         gameWindow.setSize(getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
         gameWindow.add(this);
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameWindow.getContentPane().setBackground(new Color(200, 235, 255));
+        //ImageIcon imageIcon = new ImageIcon(getClass().getResource("screen.jpg"));
+        gameWindow.setVisible(true);
+        jopInput = JOptionPane.showInputDialog("Version 0.12?");
+        //gameWindow.setPreferredSize(new Dimension(200, 300));
+        //gameWindow.getContentPane().setBackground(new Color(200, 235, 255));
+        gameWindow.setSize(getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
+        gameWindow.add(this);
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.setVisible(true);
         paintTicker.start();
-        sonar();
-    }
+        System.out.println("pin 1 going high");
+        pinA.high();
+        System.out.println("pin 1 going low");
+        pinA.low();
 
-    public void sonar()
-    {
-        System.out.println("Starting Sonar");
-//        echoLeft.addListener(new GpioPinListenerDigital()
-//        {
-//            @Override
-//            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
-//            {
-//                if (event.getState() == PinState.HIGH)
-//                {
-//                }
-//                if (event.getState() == PinState.LOW)
-//                {
-//                }
-//
-//            }
-//        });
+//        ImageIcon imageIcon = new ImageIcon(getClass().getResource("screen.jpg"));
+//        JLabel label = new JLabel(imageIcon);
+//        gameWindow.add(label);
     }
-
 
     public void paint(Graphics g)
     {
-        Graphics2D g2 = (Graphics2D) g;
-        g2.fillOval(40, 40, 10, 10);
-        g2.drawString(jopInput, 150, 150);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.fillOval(getWidth()/2, getHeight()/2, 50, 50);
     }
-
     @Override
     public void actionPerformed(ActionEvent e)
     {
