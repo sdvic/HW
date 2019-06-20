e1d0397ec1fe4e868685622887a2b30e82a08b57
Vic Wintriss <sdvic@me.com>
2019-06-19 17:44:14 -0700

    version 99.45...Pulse output OK...1 mhz...partial graphics

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,74 +1,109 @@
 import com.pi4j.io.gpio.*;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import static java.awt.Toolkit.getDefaultToolkit;
 
 /***********************************************************************
- * Full Swing Golf Strip Test version 99.27, 6/18/2019
+ * Full Swing Golf Strip Test version 99.45, 6/19/2019
  * copyright 2019 Vic Wintriss
  ***********************************************************************/
-public class Main extends JComponent implements ActionListener
+public class Main extends JComponent implements ActionListener, Runnable
 {
-    private static long start;
-    private static String jopInput;
-    private static Timer paintTicker;
-    private final static long INTERVAL = 10;
-    private final static GpioController gpio = GpioFactory.getInstance();
-    private final static GpioPinDigitalOutput pinA = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
-    private static Image screen;
-    private  static GpioPinDigitalOutput pin3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);
-    private  static GpioPinInput pin5 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03);
-    private  static GpioPinDigitalOutput pin7 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
-    private  static  GpioPinInput pin29 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05);
-    private static final GpioPinDigitalOutput pin16 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.LOW);
-    private static JFrame gameWindow;
+    private final GpioController gpio = GpioFactory.getInstance();
+    private final GpioPinDigitalOutput pinA = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+    private String version = "99.45";
+    JFrame display = new JFrame("FSG StripTest ver " + version);
+    private long start;
+    private String jopInputText;
+    private Timer paintTicker;
+    private long INTERVAL = 1000;
+    private GpioPinDigitalOutput pin3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);
+    private GpioPinInput pin5 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03);
+    private GpioPinDigitalOutput pin7 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
+    private JButton runButton = new JButton("RUN");
+    private int screenWidth = getDefaultToolkit().getScreenSize().width;
+    private int screenHeight = getDefaultToolkit().getScreenSize().height;
 
-    private void main(String[] args) throws InterruptedException
+    public static void main(String[] args)
     {
-       new Main().getGoing();
+        SwingUtilities.invokeLater(new Main());
     }
 
-    public void getGoing()
+    @Override
+    public void run()
     {
-        System.out.println("<--Pi4J--> GPIO Control Example 99.27... started.");
+
+
+        //System.out.println("Please enter operator badge number " + version);
         paintTicker = new Timer(20, this);
-        screen = getDefaultToolkit().getImage("screen.jpg");
-        jopInput = JOptionPane.showInputDialog("What do you want me to do for rev 99.26?");
-        JFrame gameWindow = new JFrame("FSG StripTest");
-        gameWindow.setSize(getDefaultToolkit().getScreenSize().width, getDefaultToolkit().getScreenSize().height);
-        gameWindow.add(this);
-        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameWindow.getContentPane().setBackground(new Color(200, 235, 255));
-        gameWindow.setVisible(true);
+        //jopInputText = JOptionPane.showInputDialog("What do you want me to do for rev " + version);
+        display.setSize(getDefaultToolkit().getScreenSize().width, getDefaultToolkit().getScreenSize().height);
+        display.add(this);
+        //display.add(runButton);
+        //runButton.addActionListener(this);
+        display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        display.getContentPane().setBackground(new Color(193, 202, 202));
+        display.setVisible(true);
+        //display.pack();
         paintTicker.start();
-//        while (true)
-//        {
-//            sleepNano();
-//            pin16.high();
-//           sleepNano();
-//            pin16.low();
-//        }
+        //while (true)
+        {
+            sleepNano();
+            // pin16.high();
+            //sleepNano();
+            //pin16.low();
+        }
     }
 
-    private static void sleepNano()
+    private void sleepNano()
     {
         start = System.nanoTime();
-        long end=0;
-        do{
+        long end = 0;
+        do
+        {
             end = System.nanoTime();
-        }while(start + INTERVAL >= end);
+        } while (start + INTERVAL >= end);
+        System.out.println("Delay in nanoseconds is: " + (end - start));
     }
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        repaint();
+        if (e.getSource() == paintTicker)
+        {
+            repaint();
+        }
+        if (e.getSource() == runButton)
+        {
+            System.out.println("you pushed run");
+        }
     }
+
     public void paint(Graphics g)
     {
-       Graphics2D g2 =(Graphics2D)g;
-       g2.drawOval(20, 20, 20, 20);
+        Graphics2D g2 = (Graphics2D) g;
+        //g2.drawString("Operator badge number " + jopInputText, 150, 150);
+        for (int i = 0; i < 16; i++)
+        {
+            g2.setFont(new Font("Bank Gothic", Font.BOLD, 16));
+            g2.drawString((i + 1) + "", (40 * i + 47), 37);
+            g2.setStroke(new BasicStroke(2));
+            g2.drawOval((40 * i + 40), 15, 30, 30);
+        }
+        g2.setStroke(new BasicStroke(.1f));
+        g2.drawLine(40, 47, (screenWidth - 60), 47);
+        g2.setColor(new Color(150, 157, 157));
+        g2.fillRect(38, 48, (screenWidth - 80), 48);
+        for (int i = 0; i < 4; i++)
+        {
+            g2.setColor(new Color(0,0,0));
+            g2.drawOval((120 * i + 180), 53, 30, 30);
+            g2.drawString((i + 1) + "", (120 * i + 190), 72);
+        }
+        g2.drawString("EMITTERS", 40, 72);
     }
 }
