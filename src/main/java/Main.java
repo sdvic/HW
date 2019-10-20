e6a048e88430fb87e0e1506c8cee5df42cc4b909
Vic Wintriss <sdvic@me.com>
2019-10-19 21:00:18 -0700

    version 102.47, 10/19/19, Blinking Alternatly OK, three GPIO pins working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,26 +8,44 @@ import com.pi4j.io.gpio.PinState;
 import javax.swing.*;
 import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_22;
 import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
-
-public class Main extends StepperMotorBase
+import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_06;
+import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_27;
+//import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_00;
+//import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
+//import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
+//import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
+// import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
+//import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
+//import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
+//import static com.pi4j.io.gpio.RaspiBcmPin.GPIO_23;
+public class Main extends StepperMotorBase implements Runnable
 {
     /**************************************************************************************
      *      Full Swing Golf Strip Test
      *      copyright 2019 Vic Wintriss
-     */     public String version = "102.26";
-    /*      October 17, 2019 Alternate Flashing lights OK, refactor
+     */
+    public String version = "102.47";
+    byte[] blinkSequence = {/* byte order (x)(x)(x)(x)(x)(36)(33)(31) */
+                            (byte) 0b00000000,
+                            (byte) 0b11111111
+    };
+    /*      October 19, 2019 Alternate Flashing lights OK, three GPIO pins working
      **************************************************************************************/
     private GpioController gpio = GpioFactory.getInstance();
     private GpioPinDigitalOutput pin33 = gpio.provisionDigitalOutputPin(GPIO_23, "RasPi pin 33", PinState.LOW);
-    private GpioPinDigitalOutput pin31 = gpio.provisionDigitalOutputPin(GPIO_22, "RasPi pin 31", PinState.LOW);
+    private GpioPinDigitalOutput pin31 = gpio.provisionDigitalOutputPin(GPIO_22, "RasPi pin 31", PinState.HIGH);
+    private GpioPinDigitalOutput pin36 = gpio.provisionDigitalOutputPin(GPIO_27, "RasPi pin 36", PinState.LOW);
+    //private GpioPinDigitalOutput pin22 = gpio.provisionDigitalOutputPin(GPIO_06, "RasPi pin 22", PinState.LOW);
+//    private GpioPinDigitalOutput pin31 = gpio.provisionDigitalOutputPin(GPIO_06, "RasPi pin 31", PinState.LOW);
+//    private GpioPinDigitalOutput pin31 = gpio.provisionDigitalOutputPin(GPIO_13, "RasPi pin 31", PinState.LOW);
+//    private GpioPinDigitalOutput pin31 = gpio.provisionDigitalOutputPin(GPIO_15, "RasPi pin 31", PinState.LOW);
+//    private GpioPinDigitalOutput pin31 = gpio.provisionDigitalOutputPin(GPIO_17, "RasPi pin 31", PinState.LOW);
+//    private GpioPinDigitalOutput pin36 = gpio.provisionDigitalOutputPin(GPIO_27, "RasPi pin 36", PinState.LOW);
     private GpioPinDigitalOutput[] pins = {
+            pin33,
             pin31,
-            pin33
-    };
-
-    byte[] blinkSequence = {/* byte order (x)(x)(x)(x)(x)(x)(33)(31) */
-            (byte) 0b00000001,
-            (byte) 0b00000010
+            pin36
+            //pin22
     };
     private GpioStepperMotorComponent motor = new GpioStepperMotorComponent(pins);
     private UserExperience UX = new UserExperience();
@@ -35,33 +53,36 @@ public class Main extends StepperMotorBase
 
     public static void main(String[] args) throws Exception
     {
-        new Main().getGoing();// Get out of static context
+        SwingUtilities.invokeLater(new Main());//To prevent interference with Swing graphics
     }
 
-    public void getGoing() throws Exception
+    @Override
+    public void run()
     {
         UX.createGUI(version);
         paintTicker.start();
         System.out.println("Starting Stepper");
-        motor.setStepInterval(1000);
+        motor.setStepInterval(100);
         motor.setStepSequence(blinkSequence);
-        motor.step(100);
+        motor.step(10000);
     }
 
-        @Override
-        public void step(long l)
-        {
-        }
+    @Override
+    public void step(long l)
+    {
+    }
 
-        @Override
-        public MotorState getState()
-        {
-            return null;
-        }
+    @Override
+    public MotorState getState()
+    {
+        return null;
+    }
 
-        @Override
-        public void setState(MotorState motorState)
-        {
-        }
+    @Override
+    public void setState(MotorState motorState)
+    {
+    }
+
+
 }
 
