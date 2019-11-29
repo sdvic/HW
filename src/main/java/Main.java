4e794d98c518f06ded444a6ca8363adfab1b82ef
vicwintriss <sdvic@me.com>
2019-11-29 15:34:17 -0700

    working on bubble text positions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main implements ActionListener
+
 {
     /****************************************************************************************
      *      Full Swing Golf Strip Test                                                      *
      *      copyright 2019 Vic Wintriss                                                     */
-     private String version = "501.160";
+     public String version = "502.15";
      /****************************************************************************************/
-    public TestSequences ts = new TestSequences();
+    //public TestSequences ts = new TestSequences();
     public UserExperience ux = new UserExperience(version);
     private boolean modeAllTest = false;
     private boolean modeTeeTest = false;
@@ -20,196 +21,176 @@ public class Main implements ActionListener
     private int testByte;
     private String codeCat;
     private int errEmitter;
-    private boolean isTestFinished = true;
+
     public static void main(String[] args)
     {
-        new Main();
+        SwingUtilities.invokeLater(
+                Main::new);
     }
     public Main()
     {
         ux.setMain(this);
-        SwingUtilities.invokeLater(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                ux.createGUI(version);
-            }
-        });
-        ts.setUx(ux);
-        ux.setTs(ts);
+
+//        ts.setUx(ux);
+//        ux.setTs(ts);
         new Timer(100, ux).start();
     }
-    public String buildDisplayErrorList(boolean[] errorList, String testSource)
-    {
-        codeCat = testSource;
-        for (int i = 0; i < errorList.length; i++)
-        {
-            if (errorList[i])
-            {
-                codeCat += (i + ", ");
-                isTestFinished = false;
-            }
-        }
-        return codeCat;
-    }
+
     public void actionPerformed(ActionEvent e)
     {
         if (e.getActionCommand().equals("ALL"))//mode 1
         {
             modeAllTest = true;
-            isTestFinished = false;
         }
         if (e.getActionCommand().equals("TEE"))//mode 2
         {
             modeTeeTest = true;
-            isTestFinished = false;
         }
-        if(e.getActionCommand().equals("SCREEN"))//mode 3
+        if (e.getActionCommand().equals("SCREEN"))//mode 3
         {
             modeScreenTest = true;
-            isTestFinished = false;
         }
-        if(e.getActionCommand().equals("SENSORS"))//mode 4
+        if (e.getActionCommand().equals("SENSORS"))//mode 4
         {
             modeSensorTest = true;
-            isTestFinished = false;
         }
         if (e.getActionCommand().equals("COMM"))//mode 5
         {
             System.out.println("COMM button pressed");
-            isTestFinished = false;
+            ux.setCommTestRunning(true);
+            System.out.println("comm key");
         }
         if (e.getActionCommand().equals("RESET"))//mode 0
         {
             System.out.println("RESET button pressed");//action 1
-            isTestFinished = false;
         }
-        if (e.getActionCommand().equals("PRINT"))
-        {
+        if (e.getActionCommand().equals("PRINT")) {
             System.out.println("PRINT button pressed");// action 2
         }
-        if (e.getActionCommand().equals("RUN"))//action 3
-        {
-            if (modeAllTest)//mode 1...ALL
-            {
-                ts.resetErrors();
-                ts.testScreen(); // run first because to resetErrors() in test.
-                testTee();
-                testSensors();
-                ts.setErrTestByteLow(1);  // byte used for testing sensors errors, bottom 8 bits   ### REMOVE ###
-                ts.setErrTestByteHigh(4); // byte used for testing sensors errors, top 8 bits   ### REMOVE ###
-                ts.setErrEmitter(2);    // byte used for testing emitter errors  ### REMOVE ###
-                ts.setErrFail(true);     // bit indicating FAIL   ### REMOVE ###
-                buildDisplayErrorList(ts.getDisplayErrorList(), "All Test Errors => ");
-            }
-            if (modeTeeTest)//mode 2...TEE
-            {
-                ts.resetErrors();
-                testTee();
-                ts.setErrFail(false);
-                errFail = ts.getErrLpClkOut() | ts.getErrRipple() | ts.getErrRclk() | ts.getErrShiftLoad();
-                buildDisplayErrorList(ts.getDisplayErrorList(), "Tee Test Errors => ");
-            }
-            if (modeScreenTest)//mode 3...SCREEN
-            {
-                ts.resetErrors();
-                ts.testScreen();
-                ts.setErrFail(false);
-                errFail = ts.getErrLpClkOut() | ts.getErrRipple() | ts.getErrRclk() | ts.getErrShiftLoad();
-                buildDisplayErrorList(ts.getDisplayErrorList(), "Screen Test Errors =>  ");
-            }
-            if (modeSensorTest)//mode 4...SENSORS
-            {
-                ts.resetErrors();
-                testSensors();
-                errFail = false;
-                //errFail = errDataOut | errSin;
-                buildDisplayErrorList(ts.getDisplayErrorList(), "Sensor Test Errors =>  ");
-            }
-            if (modeBasicTest)//mode 5...COMM?
-            {
-                testByte = (byte) 0b10101110; // byte used for testing sensors. Active low, LSB is D1
-                for (int i = 0; i < 200; i++)
-                {
-                    ts.resetErrors();
-                    testBasic();
-                    try { Thread.sleep(100); }   // 1000 milliseconds is one second.
-                    catch(InterruptedException ex) { Thread.currentThread().interrupt(); }
-                }
-                buildDisplayErrorList(ts.getDisplayErrorList(), "Basic Test Errors => ");
-            }
-            ux.setErrEmitter(ts.getErrEmitter());
-            ux.setTestFinished(isTestFinished);
-            ux.getErrorCodeDisplayField().setText(codeCat);
-            ux.setErrEmitter(errEmitter);
-        }
     }
-    private void testTee()// Set CPLD state machine to the tee frame and test all the emitters...mode 2
-    {
-        ts.resetErrors();
-        for (int i = 1; i < 5; i++)
-        {
-            ts.resetSequence();        // t1-t2
-            ts.teeSequence();          // t3-t8
-            ts.emitterSelSequence();   // t9-t14
-            ts.emitterFireSequence(i); // t15-t18
-            ts.resetSequence();        // t1-t2
-        }
-    }
-    private void testSensors()// Test each individual IR photodiode for correct operation...mode 4
-    {
-        ts.resetErrors();
-        for (int i = 0; i < 8; i++) // walking 1 test pattern
-        {
-            ts.resetSequence();      // t1-t2
-            ts.teeSequence();        // t3-t8
-            ts.emitterSelSequence(); // t9-t14
-            testByte = 128;
-            testByte = testByte >> i;
-            ts.loadTestWord(testByte);
-            ts.emitterFireSequence(0);          // t15-t18
-            ts.teeShiftOutSequence(false); // t19-t54
-            ts.resetSequence();                // t55-t56
-        }
-        for (int i = 0; i < 8; i++) // walking 0 test pattern
-        {
-            ts.resetSequence();      // t1-t2
-            ts.teeSequence();        // t3-t8
-            ts.emitterSelSequence(); // t9-t14
-            testByte = 128;
-            testByte = testByte >> i;
-            testByte = ~testByte;
-            ts.loadTestWord(testByte);
-            ts.emitterFireSequence(0);          // t15-t18
-            ts.teeShiftOutSequence(false); // t19-t54
-            ts.resetSequence();                // t55-t56
-        }
-    }
-    private void testBasic() // Test the majority of the Comm Board functionality. Uses testByteHigh, testByteLow, emitter, Sin...mode 5
-    {
-        ts.resetErrors();
-        ts.loadTestWord(testByte);
-        // Test in tee frame mode with on-board emitter
-        ts.resetSequence();
-        ts.teeSequence();
-        ts.emitterSelSequence();
-        ts.emitterFireSequence(0);
-        ts.teeShiftOutSequence(false);
-        // Test in tee frame mode with next board emitter
-        ts.resetSequence();
-        ts.teeSequence();
-        ts.emitterDeselSequence();
-        ts.emitterFireSequence(1);
-        ts.teeShiftOutSequence(true);
-        // Test the screen frame connections
-        ts.resetSequence();
-        ts.screenSequence();
-        ts.emitterSelSequence();
-        ts.emitterFireSequence(2);
-        ts.screenShiftOutSequence();
-        // End of testing
-        ts.resetSequence();
-    }
+
+
+//        if (e.getActionCommand().equals("RUN"))//action 3
+//        {
+//            if (modeAllTest)//mode 1...ALL
+//            {
+//                ts.resetErrors();
+//                ts.testScreen(); // run first because to resetErrors() in test.
+//                testTee();
+//                testSensors();
+//                ts.setErrTestByteLow(1);  // byte used for testing sensors errors, bottom 8 bits   ### REMOVE ###
+//                ts.setErrTestByteHigh(4); // byte used for testing sensors errors, top 8 bits   ### REMOVE ###
+//                ts.setErrEmitter(2);    // byte used for testing emitter errors  ### REMOVE ###
+//                ts.setErrFail(true);     // bit indicating FAIL   ### REMOVE ###
+//                buildDisplayErrorList(ts.getDisplayErrorList(), "All Test Errors => ");
+//            }
+//            if (modeTeeTest)//mode 2...TEE
+//            {
+//                ts.resetErrors();
+//                testTee();
+//                ts.setErrFail(false);
+//                errFail = ts.getErrLpClkOut() | ts.getErrRipple() | ts.getErrRclk() | ts.getErrShiftLoad();
+//                buildDisplayErrorList(ts.getDisplayErrorList(), "Tee Test Errors => ");
+//            }
+//            if (modeScreenTest)//mode 3...SCREEN
+//            {
+//                ts.resetErrors();
+//                ts.testScreen();
+//                ts.setErrFail(false);
+//                errFail = ts.getErrLpClkOut() | ts.getErrRipple() | ts.getErrRclk() | ts.getErrShiftLoad();
+//                buildDisplayErrorList(ts.getDisplayErrorList(), "Screen Test Errors =>  ");
+//            }
+//            if (modeSensorTest)//mode 4...SENSORS
+//            {
+//                ts.resetErrors();
+//                testSensors();
+//                errFail = false;
+//                //errFail = errDataOut | errSin;
+//                buildDisplayErrorList(ts.getDisplayErrorList(), "Sensor Test Errors =>  ");
+//            }
+//            if (modeBasicTest)//mode 5...COMM?
+//            {
+//                testByte = (byte) 0b10101110; // byte used for testing sensors. Active low, LSB is D1
+//                for (int i = 0; i < 200; i++)
+//                {
+//                    ts.resetErrors();
+//                    testBasic();
+//                    try { Thread.sleep(100); }   // 1000 milliseconds is one second.
+//                    catch(InterruptedException ex) { Thread.currentThread().interrupt(); }
+//                }
+//                buildDisplayErrorList(ts.getDisplayErrorList(), "Basic Test Errors => ");
+//            }
+//            ux.setErrEmitter(ts.getErrEmitter());
+//            ux.setTestFinished(isTestFinished);
+//            ux.getErrorCodeDisplayField().setText(codeCat);
+//            ux.setErrEmitter(errEmitter);
+//        }
+//    }
+//    private void testTee()// Set CPLD state machine to the tee frame and test all the emitters...mode 2
+//    {
+//        ts.resetErrors();
+//        for (int i = 1; i < 5; i++)
+//        {
+//            ts.resetSequence();        // t1-t2
+//            ts.teeSequence();          // t3-t8
+//            ts.emitterSelSequence();   // t9-t14
+//            ts.emitterFireSequence(i); // t15-t18
+//            ts.resetSequence();        // t1-t2
+//        }
+//    }
+//    private void testSensors()// Test each individual IR photodiode for correct operation...mode 4
+//    {
+//        ts.resetErrors();
+//        for (int i = 0; i < 8; i++) // walking 1 test pattern
+//        {
+//            ts.resetSequence();      // t1-t2
+//            ts.teeSequence();        // t3-t8
+//            ts.emitterSelSequence(); // t9-t14
+//            testByte = 128;
+//            testByte = testByte >> i;
+//            ts.loadTestWord(testByte);
+//            ts.emitterFireSequence(0);          // t15-t18
+//            ts.teeShiftOutSequence(false); // t19-t54
+//            ts.resetSequence();                // t55-t56
+//        }
+//        for (int i = 0; i < 8; i++) // walking 0 test pattern
+//        {
+//            ts.resetSequence();      // t1-t2
+//            ts.teeSequence();        // t3-t8
+//            ts.emitterSelSequence(); // t9-t14
+//            testByte = 128;
+//            testByte = testByte >> i;
+//            testByte = ~testByte;
+//            ts.loadTestWord(testByte);
+//            ts.emitterFireSequence(0);          // t15-t18
+//            ts.teeShiftOutSequence(false); // t19-t54
+//            ts.resetSequence();                // t55-t56
+//        }
+//    }
+//    private void testBasic() // Test the majority of the Comm Board functionality. Uses testByteHigh, testByteLow, emitter, Sin...mode 5
+//    {
+//        ts.resetErrors();
+//        ts.loadTestWord(testByte);
+//        // Test in tee frame mode with on-board emitter
+//        ts.resetSequence();
+//        ts.teeSequence();
+//        ts.emitterSelSequence();
+//        ts.emitterFireSequence(0);
+//        ts.teeShiftOutSequence(false);
+//        // Test in tee frame mode with next board emitter
+//        ts.resetSequence();
+//        ts.teeSequence();
+//        ts.emitterDeselSequence();
+//        ts.emitterFireSequence(1);
+//        ts.teeShiftOutSequence(true);
+//        // Test the screen frame connections
+//        ts.resetSequence();
+//        ts.screenSequence();
+//        ts.emitterSelSequence();
+//        ts.emitterFireSequence(2);
+//        ts.screenShiftOutSequence();
+//        // End of testing
+//        ts.resetSequence();
+//    }
 }
 
--- src/main/java/UserExperience.java
+++ src/main/java/UserExperience.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.font.FontRenderContext;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 import static java.awt.Toolkit.getDefaultToolkit;
 
@@ -11,8 +13,17 @@ public class UserExperience extends JComponent implements ActionListener
     private Main main;
 
     Timer paintTicker = new Timer(100, this);
-    private TestSequences ts;
+   // private TestSequences ts;
     private int screenWidth = getDefaultToolkit().getScreenSize().width;
+    private int screenHeight = getDefaultToolkit().getScreenSize().height;
+    private int leftMargin = screenWidth/20;
+    private int middleMargin = screenWidth/2;
+    private int rightMargin = screenWidth - screenWidth/10;
+    private int sensorBubblePitch = screenWidth/16;
+    private int emitterBubblePitch = screenWidth/8;
+    private int emitterRowYpos = screenHeight/10;
+    private int sensorRowYpos = screenHeight/120;
+
     private JButton allButton = new JButton("ALL");
     private JButton teeButton = new JButton("TEE");
     private JButton screenButton = new JButton("SCREEN");
@@ -23,28 +34,24 @@ public class UserExperience extends JComponent implements ActionListener
     private JButton printButton = new JButton("PRINT");
     private JTextField passFailTextField = new JTextField();
     private JTextField errorCodeDisplayField = new JTextField();
-    private JFrame display = new JFrame();
-    private int leftMargin = 40;
-    private int middleMargin = 250;
-    private int rightMargin = 500;
-    private Font buttonFont = new Font("Bank Gothic", Font.BOLD, 21);
+    private JFrame display;
+
+    private Font buttonFont = new Font("Bank Gothic", Font.BOLD, 15);
     private Font passFailFont = new Font("Bank Gothic", Font.BOLD, 45);
-    private String version;
-    private Ellipse2D.Double[] emitterBubbleArray = new Ellipse2D.Double[4];
+    private Ellipse2D.Double[] emitterBubbleArray = new Ellipse2D.Double[8];
     private Ellipse2D.Double[] sensorBubbleArray = new Ellipse2D.Double[16];
     private int errBit; // Error bit position
     private int errEmitter = 2;      // byte used for emitter errors
     private boolean isPass;
-    private boolean isTestFinished;
+    private boolean isCommTestRunning;
     private BasicStroke bubbleStroke = new BasicStroke(4.0f);
+    private int bubbleDiameter = 30;
+
     public UserExperience(String version)
     {
-        this.version = version;
+        display = new JFrame(version);
         paintTicker.start();
-    }
 
-    public void createGUI(String version)
-    {
         display.setSize(getDefaultToolkit().getScreenSize().width, getDefaultToolkit().getScreenSize().height);
         display.add(this);//Adds Graphics
         display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -59,6 +66,7 @@ public class UserExperience extends JComponent implements ActionListener
         teeButton.setHorizontalAlignment(SwingConstants.CENTER);
         teeButton.addActionListener(main);
         display.add(teeButton);
+
         screenButton.setBounds(leftMargin, 198, 150, 34); // SCREEN button
         screenButton.setHorizontalAlignment(SwingConstants.CENTER);
         screenButton.addActionListener(main);
@@ -86,6 +94,7 @@ public class UserExperience extends JComponent implements ActionListener
         commButton.setBounds(middleMargin, 108, 150, 34); // COMM button
         commButton.setHorizontalAlignment(SwingConstants.CENTER);
         commButton.addActionListener(main);
+        display.add(commButton);
 
         resetButton.setBounds(leftMargin, 345, 120, 58); // RESET button
         resetButton.setHorizontalAlignment(SwingConstants.CENTER);
@@ -97,67 +106,81 @@ public class UserExperience extends JComponent implements ActionListener
         printButton.addActionListener(main);
         display.add(printButton);
 
-        display.add(commButton);
         display.setSize(getDefaultToolkit().getScreenSize().width, getDefaultToolkit().getScreenSize().height);
         display.add(this);
         display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        display.getContentPane().setBackground(new Color(160, 160, 160));
-        display.setTitle("FSG StripTest ver " + version);
+        display.getContentPane().setBackground(Color.LIGHT_GRAY);
         display.setVisible(true);
 
-        for (int i = 0; i < sensorBubbleArray.length; i++)// Draw 16 sensor indicators
+        for (int i = 0; i < sensorBubbleArray.length; i++)// Load 16 sensor indicators
         {
-            sensorBubbleArray[i] = new Ellipse2D.Double((42 * i + 26), 10, 30, 30);
+            sensorBubbleArray[i] = new Ellipse2D.Double((sensorBubblePitch * i), sensorRowYpos, bubbleDiameter, bubbleDiameter);
         }
-        for (int i = 0; i < emitterBubbleArray.length; i++)
+        for (int i = 0; i < emitterBubbleArray.length; i++)//Load 8 emitter indicators
         {
-            emitterBubbleArray[i] = new Ellipse2D.Double((120 * i + 180), 59, 30, 30);
+            emitterBubbleArray[i] = new Ellipse2D.Double(emitterBubblePitch * i, emitterRowYpos, bubbleDiameter, bubbleDiameter);
         }
     }
    public void paint(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        for (int sensorNumber = 0; sensorNumber < sensorBubbleArray.length; sensorNumber++)
+        String s = "";
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setFont(buttonFont);
+        g2.setStroke(bubbleStroke);
+        FontRenderContext frc = g2.getFontRenderContext();
+
+        if (isCommTestRunning)
         {
-            g2.setStroke(bubbleStroke);
-            g2.draw(sensorBubbleArray[sensorNumber]);
-            g2.drawString("" + sensorNumber++, (int) (sensorBubbleArray[sensorNumber].getCenterX() - buttonFont.getSize()/4), (int) (sensorBubbleArray[sensorNumber].getCenterY() + buttonFont.getSize()/4));
+            commButton.setBackground(Color.BLUE);
         }
-        for (int emitterNumber = 0; emitterNumber < emitterBubbleArray.length; emitterNumber++)
+        for (int i = 0; i < sensorBubbleArray.length; i++)
         {
-            g2.setStroke(bubbleStroke);
-            g2.draw(emitterBubbleArray[emitterNumber]);
+            g2.draw(sensorBubbleArray[i]);
+            s = "" + i;
+            Rectangle2D bounds = g2.getFont().getStringBounds(s, frc);
+            float width = (float) bounds.getWidth();
+            if (width > 10)
+            {
+                width = width/4;
+            }
+            g2.drawString(s, (int) ((sensorBubblePitch * i) + width), sensorRowYpos);
+        }
+        for (int i = 0; i < emitterBubbleArray.length; i++)
+        {
             errBit = 1;
-            errBit = errBit << emitterNumber;
+            errBit = errBit << i;
             errBit = errEmitter & errBit; // current error masked
             if (errBit == 0)//Pass
             {
                 g2.setColor(Color.GREEN); // Pass green
-                g2.fill(emitterBubbleArray[emitterNumber]);
+                g2.fill(emitterBubbleArray[i]);
             }
             if (errBit == 1)//Fail
             {
                 g2.setColor(Color.RED); // Failed red
-                g2.fill(emitterBubbleArray[emitterNumber]);
+                g2.fill(emitterBubbleArray[i]);
             }
-            g2.drawString("" + emitterNumber++, (int)emitterBubbleArray[emitterNumber].getCenterX() - buttonFont.getSize()/4, (int)emitterBubbleArray[emitterNumber].getCenterY() + buttonFont.getSize()/4);
+            g2.setStroke(bubbleStroke);
+            g2.setColor(Color.BLACK);
+            g2.draw(emitterBubbleArray[i]);
+            s = i + "";
+            Rectangle2D bounds = g2.getFont().getStringBounds(s, frc);
+            float width = (float) bounds.getWidth();
+            g2.drawString(s, (int) ((emitterBubblePitch * i) + width), emitterRowYpos);
         }
     }
     public void actionPerformed(ActionEvent e)
     {
         repaint();
     }
-    public void setTs(TestSequences ts)
-    {
-        this.ts = ts;
-    }
+//    public void setTs(TestSequences ts)
+//    {
+//        this.ts = ts;
+//    }
     public void setMain(Main main)
     {
         this.main = main;
-    }
-    public void setTestFinished(boolean isTestFinished)
-    {
-        this.isTestFinished = isTestFinished;
     }
     public JTextField getErrorCodeDisplayField()
     {
@@ -167,10 +190,13 @@ public class UserExperience extends JComponent implements ActionListener
     {
         isPass = pass;
     }
-
     public void setErrEmitter(int errEmitter)
     {
         this.errEmitter = errEmitter;
+    }
+    public void setCommTestRunning(boolean commTestRunning)
+    {
+        isCommTestRunning = commTestRunning;
     }
 }
 
